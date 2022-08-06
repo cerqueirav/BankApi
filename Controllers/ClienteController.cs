6557b6a691caa4c7330d22af5ca5f76fb2adb925
Vilson <Vilson@DESKTOP-QT6SL3S>
2022-08-06 19:48:00 -0300

    Create controller Client

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,53 @@
+﻿using BankApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController
+    {
+        static List<Cliente> listaClientes = new List<Cliente>();
+
+        static int Id = 1;
+
+        [HttpPost]
+        public void Create([FromBody] Cliente cliente)
+        {
+            cliente.Id = Id++;
+            listaClientes.Add(cliente);
+        }
+
+        [HttpGet]
+        public List<Cliente> Reader()
+        {
+            return listaClientes;
+        }
+
+        [HttpGet("{id:int}")]
+        public Cliente Reader(int id) 
+        {
+            return listaClientes.FirstOrDefault(cliente => cliente.Id.Equals(id));
+        }
+
+        [HttpPut("{id:int}")]
+        public void Update(int id, [FromBody] Cliente novoCliente)
+        {
+            var cliente = listaClientes.FirstOrDefault(cliente => cliente.Id.Equals(id));
+
+            cliente.Nome = novoCliente.Nome;
+            cliente.Cpf = novoCliente.Cpf;
+        }
+
+        [HttpDelete("{id:int}")]
+        public void Delete(int id)
+        {
+            var cliente = listaClientes.FirstOrDefault(cliente => cliente.Id.Equals(id));
+
+            if(cliente is not null)
+                listaClientes.Remove(cliente);
+        }
+
+
+    }
+}
--- Models/Conta.cs
+++ Models/Conta.cs
@@ -1,12 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace BankApi.Models
 {
     public class Conta
     {
-
+        [JsonIgnore]
         public int Id { get; set; }
-        public string Cliente { get; set; }
-        public double Saldo { get; set; }
-
-        
+        public Cliente? Cliente { get; set; }
+        public double Saldo { get; set; }   
     }
 }
